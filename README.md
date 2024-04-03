5e5e2c9188eff11b8f9a8cebf505633846cd0fcb
iliaquatali <liaquatali996633@gmail.com>
2024-04-03 11:01:11 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+<p align="center">
+    <h1 align="center">Laravel 10 REST API using Passport Authentication</h1>
+</p>
+
+
+### Postman application
+
+Email: `admin@gmail.com`, Password: `admin123`.
+
+ <!-- 1. Online demo: [pos.khmernokor.com](https://pos.khmernokor.com/) -->
+
+### Screenshots
+
+#### Product list
+
+![Product list](https://raw.githubusercontent.com/angkosal/laravel-pos/master/screenshots/products_list.png)
+
+#### Create order
+
+![Create order](https://raw.githubusercontent.com/angkosal/laravel-pos/master/screenshots/pos.png)
+
+#### Order list
+
+![Order list](https://raw.githubusercontent.com/angkosal/laravel-pos/master/screenshots/order_list.png)
+
+#### Customer list
+
+![Customer list](https://raw.githubusercontent.com/angkosal/laravel-pos/master/screenshots/customer_list.png)
---
+++ routes/api.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Auth\LoginRegisterController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Public routes of authtication
+Route::controller(LoginRegisterController::class)->group(function() {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+});
+
+// Public routes of product
+Route::controller(ProductController::class)->group(function() {
+    Route::get('/products', 'index');
+    Route::get('/products/{id}', 'show');
+    Route::get('/products/search/{name}', 'search');
+});
+
+// Protected routes of product and logout
+Route::middleware('auth:api')->group( function () {
+    Route::post('/logout', [LoginRegisterController::class, 'logout']);
+
+    Route::controller(ProductController::class)->group(function() {
+        Route::post('/products', 'store');
+        Route::post('/products/{id}', 'update');
+        Route::delete('/products/{id}', 'destroy');
+    });
+});
